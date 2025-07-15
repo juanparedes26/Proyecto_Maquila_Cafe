bd8a249333cbb535d5ffa00988fc803d1267037b
juanparedes26 <139470808+juanparedes26@users.noreply.github.com>
2025-07-15 10:02:14 -0500

    Base de datos lista

--- backend/app/modelView.py
+++ backend/app/modelView.py
@@ -1,10 +1,10 @@
 from flask_admin.contrib.sqla import ModelView
-from app.models import User  # importa tus modelos aquí
-
+from app.models import User
 
 class UserAdmin(ModelView):
-    form_excluded_columns = ['password']
-    form_excluded_columns = ['password']   # Oculta el campo en el formulario
+    column_list = ('id', 'name', 'email')           # Solo muestra estos campos en la lista
+    column_exclude_list = ['password']              # Oculta 'password' en la lista y detalles
+    form_excluded_columns = ['password']            # Oculta 'password' en el formulario
 
 def register_admin_views(admin, db):
     admin.add_view(UserAdmin(User, db.session))
